c958314f844101e444ff284862e70337e4951473
sanidhyy <sanidhya.verma12345@gmail.com>
2023-10-05 10:04:31 +0000

    search bar filters created

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,18 @@
 "use client"
 
 import { useState } from "react"
+import Image from "next/image"
 import { SearchManufacturer } from "./"
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image src="/magnifying-glass.svg" alt="magnifying glass" width={40} height={40} className="object-contain" />
+  </button>
+)
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('')
+  const [model, setModel] = useState('')
   
   const handleSearch = () => {}
   
@@ -12,7 +20,16 @@ const SearchBar = () => {
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image src="/model-icon.png" width={25} height={25} className="absolute w-[20px] h-[20px] ml-4" alt="car model" />
+
+        <input type="text" name="model" value={model} onChange={(e) => e.target.value} placeholder="Tiguan" className="searchbar__input" />
+
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+    <SearchButton otherClasses="max-sm:hidden" />
     </form>
   )
 }
