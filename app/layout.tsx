4908ecb1b2d47302e82b0ec0ca18b090f2ef2d0c
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-10-06 19:21:58 +0530

    [pwa] manifest added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,15 @@
+import { Metadata } from "next";
+
 import { Footer, Navbar } from "@/components";
 import "./globals.css";
-
-// metadata interface
-interface Metadata {
-  title: string;
-  description: string;
-  author: string;
-  themeColor: string;
-}
 
 // metadata
 export const metadata: Metadata = {
   title: "Car Hub",
   description: "Discover the best cars in the world",
-  author: "Sanidhya Kr. Verma",
+  authors: [{ name: "Sanidhya Kr. Verma", url: "https://github.com/sanidhyy" }],
   themeColor: "#2B59FF",
+  manifest: "/manifest.json",
 };
 
 // page layout
